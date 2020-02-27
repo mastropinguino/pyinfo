a1f330d6c13bd7e70191f171f8d1a63beceffa62
Alex Thomae <alex@thom.ae>
2020-02-27 16:12:33 +0100

    Check for support for the deprecated `platform.dist()` method

--- pyinfo/_pyinfo.py
+++ pyinfo/_pyinfo.py
@@ -70,7 +70,7 @@ def collect_system_info():
     if hasattr(sys, 'subversion'):
         data['Python Subversion'] = ', '.join(sys.subversion)
 
-    if platform.dist()[0] != '' and platform.dist()[1] != '':
+    if hasattr(platform, 'dist') and platform.dist()[0] != '' and platform.dist()[1] != '':
         osversion = '{} {} ({} {})'.format(platform.system(),
                                            platform.release(),
                                            platform.dist()[0].capitalize(),
